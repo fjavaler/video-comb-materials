7b89dd218d369b2f197aa753081f11eb91781161
Frederick Javalera <frederickjavalera@gmail.com>
2021-12-10 14:22:19 -0700

    Completed.

--- 24-debounce-and-throttle/starter/Starter.playground/Pages/Debounce.xcplaygroundpage/Contents.swift
+++ 24-debounce-and-throttle/starter/Starter.playground/Pages/Debounce.xcplaygroundpage/Contents.swift
@@ -4,7 +4,36 @@ import PlaygroundSupport
 
 //: [Next](@next)
 
+let subject = PassthroughSubject<String, Never>()
 
+let debounced = subject
+  .debounce(for: .seconds(1.0), scheduler: DispatchQueue.main)
+  .share()
+
+let subjectTimeline = TimelineView(title: "Emitted values")
+let debouncedTimeline = TimelineView(title: "Debounced values")
+
+let view = VStack(spacing: 100) {
+  subjectTimeline
+  debouncedTimeline
+}.frame(width: 300, height: 300)
+
+PlaygroundPage.current.liveView =  UIHostingController(rootView: view)
+
+subject.displayEvents(in: subjectTimeline)
+debounced.displayEvents(in: debouncedTimeline)
+
+let subscription1 = subject
+  .sink { string in
+    print("+\(deltaTime)s: Subject emitted: \(string)")
+  }
+
+let subscription2 = debounced
+  .sink { string in
+    print("+\(deltaTime)s: Subject emitted: \(string)")
+  }
+
+subject.feed(with: typingHelloWorld)
 
 /*:
  Copyright (c) 2019 Razeware LLC

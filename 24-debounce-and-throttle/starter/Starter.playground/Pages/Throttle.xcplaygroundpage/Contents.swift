3814808a659c9c5b5915b8810e6a1f5c3c968061
Frederick Javalera <frederickjavalera@gmail.com>
2021-12-10 14:35:05 -0700

    Completed Throttle.

--- 24-debounce-and-throttle/starter/Starter.playground/Pages/Throttle.xcplaygroundpage/Contents.swift
+++ 24-debounce-and-throttle/starter/Starter.playground/Pages/Throttle.xcplaygroundpage/Contents.swift
@@ -1,8 +1,40 @@
 import Combine
 import SwiftUI
 import PlaygroundSupport
-
 //: [Next](@next)
+
+let throttleDelay = 1.0
+
+let subject = PassthroughSubject<String, Never>()
+
+let throttled = subject
+  .throttle(for: .seconds(throttleDelay), scheduler: DispatchQueue.main, latest: false)
+  .share()
+
+let subjectTimeline = TimelineView(title: "Emitted values")
+let throttledTimeline = TimelineView(title: "Throttled values")
+
+let view = VStack(spacing: 100) {
+  subjectTimeline
+  throttledTimeline
+}.frame(width: 300, height: 300)
+
+PlaygroundPage.current.liveView = UIHostingController(rootView: view)
+
+subject.displayEvents(in: subjectTimeline)
+throttled.displayEvents(in: throttledTimeline)
+
+let subscription1 = subject
+  .sink { string in
+    print("+\(deltaTime)s: Subject emitted: \(string)")
+  }
+
+let subscription2 = throttled
+  .sink { string in
+    print("+\(deltaTime)s: Throttle emitted: \(string)")
+  }
+
+subject.feed(with: typingHelloWorld)
 /*:
  Copyright (c) 2019 Razeware LLC
  
